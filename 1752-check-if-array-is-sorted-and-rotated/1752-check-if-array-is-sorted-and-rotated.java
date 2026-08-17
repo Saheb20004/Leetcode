7ce515c03c9cd09caef896a8b5addcd4a9dd3f42
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-17 20:24:52 +0530

    Time: 4 ms (2.75%), Space: 43.5 MB (12.27%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,29 +1,20 @@
-// Brute Force - O(n * n)
+// Approach - O(n * n)
 
 class Solution {
     public boolean check(int[] nums) {
         int n = nums.length;
+        // Create a sorted copy
+        int[] sorted = nums.clone();
+        Arrays.sort(sorted);
 
         for (int rotations = 0; rotations <= n; rotations++) {
 
-            int sorted[] = new int[n];
-            int idx = 0;
-
-            // Take elements from rotations to end
-            for (int i = rotations; i < n; i++) {
-                sorted[idx++] = nums[i];
-            }
-
-            // Take elements from beginning to rotations-1
-            for (int i = 0; i < rotations; i++) {
-                sorted[idx++] = nums[i];
-            }
-
-            // Check if rotated array is sorted
             boolean isSorted = true;
 
-            for (int i = 1; i < n; i++) {
-                if (sorted[i - 1] > sorted[i]) {
+            // Compare sorted array with rotated nums
+            for (int i = 0; i < n; i++) {
+
+                if (sorted[i] != nums[(i + rotations) % n]) {
                     isSorted = false;
                     break;
                 }
