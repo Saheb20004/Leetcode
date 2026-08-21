1aed39f6691db2e5c32e6c2686a54619b5879df5
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-22 00:35:21 +0530

    Time: 2 ms (99.82%), Space: 48.9 MB (25.61%) - LeetHub

--- 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
+++ 0922-sort-array-by-parity-ii/0922-sort-array-by-parity-ii.java
@@ -1,34 +1,24 @@
 
-// Brute Force Approach
+// Optimal Approach
 
 class Solution {
     public int[] sortArrayByParityII(int[] nums) {
         int n=nums.length;
-        // Create 2 Arrays to store EVEN and ODD numbers separately
-        int even[]=new int[n/2];
-        int odd[]=new int[n/2];
+        int ans[]=new int[n];
 
-        // Traverse the nums array and store the EVEN and ODD numbers separately
-        int j=0,k=0;
+        int evenIdx=0;
+        int oddIdx=1;
+
         for(int i=0;i<n;i++){
             if(nums[i] % 2 == 0){
-                even[j++]=nums[i];
+                ans[evenIdx]=nums[i];
+                evenIdx += 2;
             }
             else{
-                odd[k++]=nums[i];
+                ans[oddIdx]=nums[i];
+                oddIdx += 2;
             }
         }
-
-        // Now Rearrange the nums Array by pushing EVEN numbers to even places and ODD numbers to odd places maintaining the insertion order
-        int a=0,b=0;
-        for(int i=0;i<n;i++){
-            if(i % 2 == 0){
-                nums[i] = even[a++];
-            }
-            else{
-                nums[i] = odd[b++];
-            }
-        }
-        return nums;
+        return ans;
     }
 }
