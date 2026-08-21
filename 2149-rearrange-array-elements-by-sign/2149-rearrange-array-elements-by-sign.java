770cb98827b5d59f830eceda969d226c941194ce
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-22 00:19:56 +0530

    Time: 3 ms (100%), Space: 175.4 MB (57.95%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.java
@@ -1,33 +1,23 @@
-// Brute Force Approach
+// Optimal Approach
 
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n=nums.length;
-        // Create 2 Arrays to store '+' and '-' numbers separately
-        int pos[]=new int[n/2];
-        int neg[]=new int[n/2];
+        int ans[]=new int[n];
 
-        // Traverse the nums array and store the '+' and '-' numbers separately
-        int j=0,k=0;
+        int posIdx=0;
+        int negIdx=1;
+
         for(int i=0;i<n;i++){
             if(nums[i] > 0){
-                pos[j++]=nums[i];
+                ans[posIdx]=nums[i];
+                posIdx += 2;
             }
             else{
-                neg[k++]=nums[i];
+                ans[negIdx]=nums[i];
+                negIdx += 2;
             }
         }
-
-        // Now Rearrange the nums Array by pushing '+' numbers to even places and '-' numbers to odd places maintaining the insertion order
-        int a=0,b=0;
-        for(int i=0;i<n;i++){
-            if(i % 2 == 0){
-                nums[i] = pos[a++];
-            }
-            else{
-                nums[i] = neg[b++];
-            }
-        }
-        return nums;
+        return ans;
     }
 }
