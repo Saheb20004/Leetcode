a89526b57be35f1165868f4dd2d01f870bdf38a4
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-21 20:39:46 +0530

    Time: 0 ms (100%), Space: 43.5 MB (42.46%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,20 +1,34 @@
-// Better Approach
+// Optimal Approach - 3 pointer Approach
+// Dutch National Flag
 
 class Solution {
     public void sortColors(int[] nums) {
         int n=nums.length;
-        int count0=0; // Track the no. of Zeros(0)
-        int count1=0; // Track the no. of Ones(1)
-        int count2=0; // Track the no. of Twos(2)
 
-        for(int i=0;i<n;i++){
-            if(nums[i] == 0) count0++;
-            else if(nums[i] == 1) count1++;
-            else count2++;
+        int mid=0;
+        int low=0;
+        int high=n-1;
+
+        while(mid <= high){
+            
+            if(nums[mid] == 0){
+                swap(nums, mid, low);
+                low++;
+                mid++;
+            }
+            else if(nums[mid] == 1){
+                mid++;
+            }
+            else{
+                swap(nums, mid, high);
+                high--;
+            }
         }
+    }
 
-        for(int i=0; i<count0; i++)  nums[i]=0;
-        for(int i=count0; i < count0+count1; i++) nums[i]=1;
-        for(int i=count0+count1; i < n; i++) nums[i]=2;
+    private void swap(int arr[],int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
