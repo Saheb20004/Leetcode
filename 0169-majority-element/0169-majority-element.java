b9e01634dbedac4e6e0565e05c93e37a35a52227
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-21 21:22:05 +0530

    Time: 1 ms (99.89%), Space: 55.8 MB (18.19%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,23 +1,31 @@
-
-// Approach using HashMap
-// Traverse the array.
-// Store each element's frequency in the map.
-// If any element's frequency becomes greater than n / 2, return it.
-
-
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        int n = nums.length;
-
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-
-            if (map.get(num) > n / 2) {
-                return num;
+        int n=nums.length;
+        int majorEl=0;
+        int count=0;
+        // 1. Apply Moore's Voting Algo
+        for(int i=0;i<n;i++){
+            if(count == 0){
+                // Assign a mojority el
+                majorEl=nums[i];
+                count=1;
+            }
+            else if(nums[i] == majorEl){
+                count++;
+            }
+            else{ // nums[i] != majorEl
+                count--;
             }
         }
 
+        // 2. Verification
+        int countMajorEl=0;
+        for(int i=0;i<n;i++){
+            if(nums[i] == majorEl) countMajorEl++;
+        }
+
+        if(countMajorEl > n/2) return majorEl;
+        // If Does not exist any Majority Element
         return -1;
     }
 }
