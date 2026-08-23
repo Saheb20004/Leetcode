d07693285c30e27a94ad3fc114ad2c646aea94ad
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-23 13:42:12 +0530

    Time: 3 ms (72.91%), Space: 52.7 MB (41.1%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,26 +1,52 @@
 
-//  Better Approach using HashMap
-// Traverse the array.
-// Store each element's frequency in the map.
-// If any element's frequency becomes greater than n / 2, return it.
-
-
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        List<Integer> ans = new ArrayList<>();
+        ArrayList<Integer> ans = new ArrayList<>();
 
-        int n = nums.length;
+        int n=nums.length;
+        int majorEl1 = Integer.MIN_VALUE, count1=0;
+        int majorEl2 = Integer.MIN_VALUE, count2=0;
 
-        for (int num : nums) {
-            map.put(num, map.getOrDefault(num, 0) + 1);
-
-            if (map.get(num) > n / 3 && !ans.contains(num)) {
-                ans.add(num);
+        // 1. Apply extended Boyer Moore's Voting Algo
+        for(int i=0;i<n;i++){
+            if(count1 == 0 && majorEl2 != nums[i]){
+                // Assign a mojority el
+                majorEl1=nums[i];
+                count1=1;
             }
-            if(ans.size() == 2) break;
+            else if(count2 == 0 && majorEl1 != nums[i]){
+                // Assign a mojority el
+                majorEl2=nums[i];
+                count2=1;
+            }
+
+
+            else if(nums[i] == majorEl1){
+                count1++;
+            }
+            else if(nums[i] == majorEl2){
+                count2++;
+            }
+
+
+            else{ // nums[i] != majorEl
+                count1 --;
+                count2 --;
+            }
         }
 
+        // 2. Verification
+        int countMajorEl1=0;
+        int countMajorEl2=0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i] == majorEl1) countMajorEl1++;
+            if(nums[i] == majorEl2) countMajorEl2++;
+        }
+
+        if(countMajorEl1 > n/3)  ans.add(majorEl1);
+        if(countMajorEl2 > n/3)  ans.add(majorEl2);
+        // If Does not exist any Majority Element
         return ans;
     }
 }
