0b52faa6e8b275c595d2da1694877e0fb94b2e76
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-23 21:12:01 +0530

    Time: 36 ms (29.6%), Space: 59.2 MB (45.47%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,46 @@
+// Optimal ( 2 pointer Approach )
+
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>();
 
-        int n=nums.length;
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums); // Sort the array ascending order
 
-        for(int i=0;i<n;i++){
-            HashSet<Integer> visited = new HashSet<>();
+        for(int i=0;i<nums.length-2;i++){
 
-            for(int j=i+1;j<n;j++){
+            if(i>0  &&  nums[i] == nums[i-1])
+                continue; //Skip duplicate element for i
 
-                int thirdEl = -(nums[i] + nums[j]);
+            int j=i+1; // Pointer 1
+            int k=nums.length-1; // Pointer 2
 
-                    if (visited.contains(thirdEl)) {
-                        List<Integer> list = new ArrayList<>();
+            while(j < k){
 
-                        list.add(nums[i]);
-                        list.add(nums[j]);
-                        list.add(thirdEl);
-                    // Sort & add the list into a Set to avoid storing duplicate list
-                        Collections.sort(list);
-                        set.add(list);
-                    }
-                    visited.add(nums[j]);
+                int sum = nums[i] + nums[j] + nums[k];
+                if(sum == 0){
+                    ans.add(Arrays.asList(nums[i],nums[j],nums[k]));
+
+                    while(j<k && nums[j] == nums[j+1]) //Skip duplicates for j
+                        j++;
+
+                    while(j<k  &&  nums[k] == nums[k-1]) //Skip duplicates for k
+                        k--;
+
+                    //Move the pointers after finding the triplets
+                    j++;
+                    k--;
+                }
+
+                else if(sum < 0){
+                    j++;// Sum is less than zero, increment j to increase the sum
+
+                }
+
+                else{
+                    k--;// Sum is less than zero, decrement k to decrease the sum
                 }
             }
-
-        ans.addAll(set);
+        }
         return ans;
     }
 }
