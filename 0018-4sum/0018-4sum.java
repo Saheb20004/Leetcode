ff4982153839b22ca507908916f291464e56d5c1
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-23 22:11:50 +0530

    Time: 21 ms (41.76%), Space: 44.9 MB (99.85%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,37 +1,55 @@
+// Optimal ( 2 pointer Approach )
 
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> ans = new ArrayList<>();
-        HashSet<List<Integer>> set = new HashSet<>();
-
         int n=nums.length;
 
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                HashSet<Long> visited = new HashSet<>();
-                for(int k=j+1;k<n;k++){
-                        long fourthEl = (long) target
-                                            - nums[i]
-                                            - nums[j]
-                                            - nums[k];
-                                        
-                        if( visited.contains(fourthEl) ){
-                            List<Integer> list = new ArrayList<>();
+        List<List<Integer>> ans=new ArrayList<>();
+        Arrays.sort(nums); // Sort the array ascending order
 
-                            list.add(nums[i]);
-                            list.add(nums[j]);
-                            list.add(nums[k]);
-                            list.add((int)fourthEl);
-                        // Sort & add the list into a Set to avoid storing duplicate list
-                            Collections.sort(list);
-                            set.add(list);
-                        }
-                    visited.add( (long) nums[k]);
+        for(int i=0;i<n-3;i++){
+
+            if(i>0  &&  nums[i] == nums[i-1])
+                continue; //Skip duplicate element for i
+
+            for(int j=i+1; j < n-2; j++){
+
+                if(j != i+1  &&  nums[j] == nums[j-1])
+                    continue; //Skip duplicate element for j
+
+                int k=j+1; // Pointer 1
+                int l=nums.length-1; // Pointer 2
+
+                while(k < l){
+
+                    long sum = (long)nums[i] + nums[j] + nums[k] + nums[l];
+                    if( sum == target ){ // Java will safely compare long with int.
+                        ans.add( Arrays.asList(nums[i],nums[j],nums[k],nums[l]) );
+
+                        while(k < l && nums[k] == nums[k+1]) //Skip duplicates for k
+                            k++;
+
+                        while(k < l  &&  nums[l] == nums[l-1]) //Skip duplicates for l
+                            l--;
+
+                        //Move the pointers after finding the quadruplets
+                        k++;
+                        l--;
+                    }
+
+                    else if(sum < target){
+                        k++;// Sum is less than zero, increment k to be close to the target
+
+                    }
+
+                    else{
+                        l--;// Sum is less than zero, decrement l to be close to the target
+                    }
+
                 }
             }
         }
 
-        ans.addAll(set);
         return ans;
     }
 }
