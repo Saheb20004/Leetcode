8d455a21f68d3eb6e2ee214af7a17ed8920bb0e3
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-23 13:17:28 +0530

    Time: 18 ms (14.21%), Space: 53.2 MB (14.03%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,31 +1,26 @@
-// Brute Force - O(n * n)
+
+//  Better Approach using HashMap
+// Traverse the array.
+// Store each element's frequency in the map.
+// If any element's frequency becomes greater than n / 2, return it.
+
 
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int n=nums.length;
+        HashMap<Integer, Integer> map = new HashMap<>();
         List<Integer> ans = new ArrayList<>();
 
-        for(int i=0;i<n;i++){
-            int count = 0;
+        int n = nums.length;
 
-            // Avoid checking the same element again
-            if (ans.contains(nums[i])) {
-                continue;
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+
+            if (map.get(num) > n / 3 && !ans.contains(num)) {
+                ans.add(num);
             }
-
-            // if(ans.size() == 0 || ans.get(0) != nums[i]) {
-            for(int j=0;j<n;j++){     
-                if(nums[j] == nums[i]){
-                    count ++;
-                }
-                if(count > n/3){
-                    ans.add(nums[j]);
-                    break;
-                }
-                }
-                if(ans.size() == 2) break;
-            // }
+            if(ans.size() == 2) break;
         }
+
         return ans;
     }
 }
