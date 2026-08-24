19fefb9342737a2e9726e6e55b86730f85296637
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-24 19:10:30 +0530

    Time: 113 ms (5.07%), Space: 47.6 MB (24.34%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,6 +1,9 @@
 // Extreme Brute Force
 // TC - O(n*n * n*n)
 // SC - O(n*n)
+// Brute Force
+// TC - O(n*n * n * n)
+// SC - O(1)
 
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
@@ -8,36 +11,32 @@ class Solution {
             int ans[] = new int[2];
             
             int n = grid.length; // n x n grid
-
-            int arr[] = new int[n * n];
-            int index = 0;
-            for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n; j++) {
-                    arr[index] = grid[i][j];
-                    index++;
-                }
-            }
+            
             int missing = 0;
             int duplicate = 0;
-            // Iterate from 1 to n and check
+            // Iterate from 1 to n*n and check
             // if the current number is present
-            for (int i = 1; i <= n*n; i++) {
+            for (int num = 1; num <= n*n; num++) {
                 int count = 0;
-                for (int j = 0; j < n*n; j++) {
-                    if (arr[j] == i) {
-                        count ++;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+                        if(grid[i][j] == num)
+                            count ++;
                     }
                 }
+
                 // If the current number is not present
                 if (count == 0)
-                    missing = i;
+                    missing = num;
                     
                 else if(count == 2)
-                    duplicate = i;
+                    duplicate = num;
             }
             
             ans[0]=duplicate;
             ans[1]=missing;
+
         return ans;
     }
 }
