44fee5a28b32529ed1ab2bd8bee3ff6530fe82ed
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-08-25 00:24:18 +0530

    Time: 1 ms (92.99%), Space: 47.5 MB (15.78%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -1,17 +1,22 @@
-// Brute Force
+// Optimal Approach - 1 (Based on Observation)
 
 class Solution {
     public int maxProduct(int[] nums) {
-        int n=nums.length;
-        int maxProd=Integer.MIN_VALUE;
-       for(int i=0;i<n;i++){
-            int prod = 1;   // reset for every i
-            
-            for(int j=i;j<n;j++){
-                prod *= nums[j];
-                maxProd = Math.max(maxProd, prod);
-            }
-       }
-       return maxProd;
+        int n = nums.length;
+
+        int prefixProd = 1;
+        int suffixProd = 1;
+        int maxProd = Integer.MIN_VALUE;
+
+        for(int i=0;i<n;i++){
+            if(prefixProd == 0) prefixProd = 1;
+            if(suffixProd == 0) suffixProd = 1;
+
+            prefixProd *= nums[i];
+            suffixProd *= nums[n-1 - i];
+
+            maxProd = Math.max(maxProd, Math.max(prefixProd, suffixProd));
+        }
+        return maxProd;
     }
 }
