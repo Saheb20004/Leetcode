ca0451e840a5b84faba2a9fa7ba18a597733b60a
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-06 17:14:16 +0530

    Time: 0 ms (100%), Space: 44.6 MB (40.28%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.java
@@ -1,13 +1,27 @@
+// Optimal using binary Search Algo
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        for(int i=0;i<arr.length;i++){
-            if(arr[i] <= k){
-                k ++;
-            }
-            else{
-                break;
+
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // Number of positive integers missing before arr[mid]
+            int missing = arr[mid] - (mid + 1);
+
+            if (missing < k) {
+                // Not enough missing numbers, so search on the right side
+                low = mid + 1;
+            } 
+            else {
+                // We have at least k missing numbers, so search on the left side
+                high = mid - 1;
             }
         }
-        return k;
+        // low = number of elements before the answer
+        // k + low || k + high + 1 gives the kth missing positive number
+        return low + k;
     }
 }
