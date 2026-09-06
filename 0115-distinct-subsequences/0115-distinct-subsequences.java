7cdc7ed401731cb22b757b878f0cabd35a6d91af
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-07 00:06:21 +0530

    Time: 19 ms (69.08%), Space: 54.5 MB (27.55%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.java
+++ 0115-distinct-subsequences/0115-distinct-subsequences.java
@@ -1,44 +1,38 @@
-// Solve using DP Memoization
-//  TC -> O(n * m),     SC -> O(n + m) + O(n * m)
+// Solve using DP Tabulatin
+//  TC -> O(n * m),     SC -> O(n * m)
 
 class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length();
         int m = t.length();
-        // dp[i][j] = answer for s[0...i] and t[0...j]
-        int[][] dp = new int[n][m];
-        // -1 means this state has not been calculated yet
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(dp[i], -1);
+
+        // dp[i][j] = number of distinct subsequences of
+        // s[0...i-1] that can form t[0...j-1]
+        int[][] dp = new int[n + 1][m + 1];
+        // Empty t can be formed from any prefix of s
+        // by deleting all characters.
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
 
-        return helperFn(n-1, m-1, s, t, dp);
-    }
+        // Fill the table
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
 
-    private int helperFn(int i, int j, String s, String t, int[][] dp) {
-        //      Base Case
-        // t is completely matched
-        if (j < 0) return 1;
-        // s is exhausted but t is still remaining
-        if (i < 0) return 0;
-        // Already calculated this state
-        if (dp[i][j] != -1) {
-            return dp[i][j];
+                if (s.charAt(i-1) == t.charAt(j-1)) {
+                    // Two choices:
+                    // 1. Take s[i-1]
+                    // 2. Don't take s[i-1]
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+
+                } 
+                else {
+                    // Characters don't match,
+                    // so skip s[i-1]
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
         }
-
-        if (s.charAt(i) == t.charAt(j)) {
-            // Take s[i] and match it with t[j]
-            int take = helperFn(i-1, j-1, s, t, dp);
-            // Don't take s[i]
-            int notTake = helperFn(i-1, j, s, t, dp);
-
-            dp[i][j] = take + notTake;
-        } 
-        else {
-            // Characters don't match, so skip s[i]
-            dp[i][j] = helperFn(i-1, j, s, t, dp);
-        }
-
-        return dp[i][j];
+        return dp[n][m];
     }
 }
