6d7762f693ac92329334399529e6c7cb64f0da73
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-07 21:25:16 +0530

    Time: 2 ms (100%), Space: 43.3 MB (70.21%) - LeetHub

--- 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
+++ 0940-distinct-subsequences-ii/0940-distinct-subsequences-ii.java
@@ -1,74 +1,43 @@
-// Memoization DP
-
-
 class Solution {
     static final int MOD = 1000000007;
 
     public int distinctSubseqII(String s) {
-        int n = s.length();
 
-        // dp[i] = number of distinct non-empty subsequences
-        // that can be formed using s[0...i]
-        int[] dp = new int[n];
+        // end[c] = number of distinct subsequences
+        // whose last character is c
+        long[] end = new long[26];
 
-        // -1 means this state has not been calculated yet
-        Arrays.fill(dp, -1);
+        for (char c : s.toCharArray()) {
 
-        return helperFn(n - 1, s, dp);
-    }
+            int ch = c - 'a';
 
-    private int helperFn(int i, String s, int[] dp) {
+            /*
+             * Every existing subsequence can be extended
+             * by the current character.
+             *
+             * Also, the current character itself forms
+             * a new subsequence.
+             *
+             * We replace end[ch] because all subsequences
+             * ending with this character become represented
+             * by the current occurrence.
+             */
+            long total = 1;
 
-        // Base Case
-        // No characters -> no non-empty subsequence
-        if (i < 0) {
-            return 0;
-        }
-
-        // Already calculated
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        /*
-         * For every subsequence formed using s[0...i-1],
-         * we have two choices:
-         *
-         * 1. Don't take s[i]
-         * 2. Take s[i]
-         *
-         * Taking s[i] creates new subsequences by appending
-         * s[i] to every previous subsequence, plus s[i] itself.
-         */
-        int notTake = helperFn(i - 1, s, dp);
-
-        int take = helperFn(i - 1, s, dp) + 1;
-
-        dp[i] = (int)((notTake + (long) take) % MOD);
-
-        /*
-         * If the same character appeared before, some of the
-         * subsequences counted by 'take' are duplicates.
-         *
-         * Find the previous occurrence of s[i].
-         */
-        int prev = -1;
-
-        for (int j = i - 1; j >= 0; j--) {
-            if (s.charAt(j) == s.charAt(i)) {
-                prev = j;
-                break;
+            for (int i = 0; i < 26; i++) {
+                total += end[i];
             }
+
+            end[ch] = total % MOD;
         }
 
-        /*
-         * Remove the duplicate subsequences that were already
-         * created when the previous occurrence was processed.
-         */
-        if (prev != -1) {
-            dp[i] = (int)((dp[i] - (long) helperFn(prev - 1, s, dp) - 1 + MOD) % MOD);
+        long answer = 0;
+
+        for (int i = 0; i < 26; i++) {
+            answer += end[i];
+            answer %= MOD;
         }
 
-        return dp[i];
+        return (int) answer;
     }
 }
