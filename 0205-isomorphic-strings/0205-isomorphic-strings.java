5e88c99f89dcc2fc916c523c46c64791f44477de
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-09 16:08:31 +0530

    Time: 26 ms (5.37%), Space: 43.7 MB (86.31%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -5,17 +5,31 @@ class Solution {
             return false;
         }
 
+        HashMap<Character, Character> mapST = new HashMap<>(); // s -> t map
+        HashMap<Character, Character> mapTS = new HashMap<>(); // t -> s map
+
         for (int i = 0; i < s.length(); i++) {
 
             char c1 = s.charAt(i);
             char c2 = t.charAt(i);
 
-            int firstS = s.indexOf(c1);
-            int firstT = t.indexOf(c2);
-
-            if (firstS != firstT) {
-                return false;
+            // Check s -> t mapping
+            if (mapST.containsKey(c1)) {
+                if (mapST.get(c1) != c2) {
+                    return false;
+                }
             }
+
+            // Check t -> s mapping
+            if (mapTS.containsKey(c2)) {
+                if (mapTS.get(c2) != c1) {
+                    return false;
+                }
+            }
+
+            // Create the mapping
+            mapST.put(c1, c2);
+            mapTS.put(c2, c1);
         }
 
         return true;
