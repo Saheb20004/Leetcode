8481eafbc14da1266ffaa7e4ad91051c2d0cad33
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-11 12:16:09 +0530

    Time: 293 ms (29.14%), Space: 47.3 MB (22.45%) - LeetHub

---
+++ 2094-finding-3-digit-even-numbers/2094-finding-3-digit-even-numbers.java
@@ -0,0 +1,33 @@
+// Brute Force Approach
+// TC -> O(n³ + k * log k) ,    k -> no. of unique 3-digit even elements
+// SC -> O(k)
+
+class Solution {
+    public int[] findEvenNumbers(int[] digits) {
+        int n = digits.length;
+        int num = 0;
+
+        Set<Integer> set = new HashSet<>();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                for(int k=0;k<n;k++){
+                    if(i == j || j == k || i == k){
+                        continue;
+                    }
+                    num = digits[i] * 100 + digits[j] * 10 + digits[k];
+                    if(num >= 100 && num % 2 == 0){ // Check for Even && no leading zeros
+                        set.add(num);
+                    }
+                }
+            }
+        }
+        int arr[]=new int[set.size()];
+        int i=0;
+        for(int el : set){
+            arr[i++] = el;
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
