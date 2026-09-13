7669c70a31470e649cdc58e32d3350fd01cd844d
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-13 15:58:01 +0530

    Time: 0 ms (100%), Space: 47.1 MB (16.48%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.java
+++ 0141-linked-list-cycle/0141-linked-list-cycle.java
@@ -9,18 +9,24 @@
  *     }
  * }
  */
+
+ // Tortoise - Hare Algo
+
 public class Solution {
     public boolean hasCycle(ListNode head) {
-        HashMap<ListNode, Integer> map = new HashMap<>();
-        
-        ListNode temp = head;
-        while(temp != null){
-            if(map.containsKey(temp)){
-                return true;
-            }
-            map.put(temp,1);
-            temp = temp.next;
+
+        if(head == null || head.next == null) return false;
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) return true; //Cycle detected
         }
-        return false;
+
+        return false;//No cycle detected
     }
 }
