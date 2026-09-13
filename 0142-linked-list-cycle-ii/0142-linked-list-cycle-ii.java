59c94f3a221f8f4f388e32eb87e7ac147ab9e5ff
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-13 16:40:55 +0530

    Time: 0 ms (100%), Space: 46.8 MB (13.62%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,16 +11,31 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        HashMap<ListNode, Integer> map = new HashMap<>();
-        
-        ListNode temp = head;
-        while(temp != null){
-            if(map.containsKey(temp)){
-                return temp;
-            }
-            map.put(temp,1);
-            temp = temp.next;
+
+        if(head == null || head.next == null) return null;
+
+        ListNode slow=head;
+        ListNode fast=head;
+
+        // Phase 1: Detect whether a cycle exists
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+
+            if(slow == fast) break; //Cycle detected
         }
-        return null;
+
+        // No cycle
+        if (fast == null || fast.next == null) {
+            return null;
+        }
+
+        // Phase 2: Find the starting point of the cycle
+        slow = head;
+         while(slow != fast){
+            slow = slow.next;
+            fast = fast.next;
+         }
+         return slow;
     }
 }
