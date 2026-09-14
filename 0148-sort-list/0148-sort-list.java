0ff47dc211b3aaf4359965bf2ce6d2e61bb24282
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-14 17:53:00 +0530

    Time: 9 ms (96.07%), Space: 59.6 MB (36.92%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -8,32 +8,73 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
 class Solution {
+
     public ListNode sortList(ListNode head) {
-        // Edge Case
-        if(head == null || head.next == null){
+
+        // Base case
+        if (head == null || head.next == null) {
             return head;
         }
-        
-        ArrayList<Integer> list = new ArrayList<>();
 
-        // Add LinkedList elements into the ArrayList
-        ListNode temp = head;
-        while(temp != null){
-            list.add(temp.val);
+        // Find middle
+        ListNode mid = findMid(head);
+
+        // Split the list
+        ListNode leftHead = head;
+        ListNode rightHead = mid.next;
+        mid.next = null;
+
+        // Sort both halves
+        ListNode left = sortList(leftHead);
+        ListNode right = sortList(rightHead);
+
+        // Merge
+        return merge(left, right);
+    }
+
+    // Finds the last node of the left half
+    private ListNode findMid(ListNode head) {
+
+        ListNode slow = head;
+        ListNode fast = head.next;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    // Merge two sorted linked lists
+    private ListNode merge(ListNode left, ListNode right) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        while (left != null && right != null) {
+
+            if (left.val <= right.val) {
+                temp.next = left;
+                left = left.next;
+            } 
+            else {
+                temp.next = right;
+                right = right.next;
+            }
+
             temp = temp.next;
         }
 
-        Collections.sort(list);
-
-        // Replace the node values with the stored list values
-        int i=0;
-        temp = head;
-        while(temp != null){
-            temp.val = list.get(i++);
-            temp = temp.next;
+        if (left != null) {
+            temp.next = left;
+        } 
+        else {
+            temp.next = right;
         }
 
-        return head;
+        return dummy.next;
     }
 }
