c8e9983a49e624badddabea18aa54c8445fac984
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-14 14:41:51 +0530

    Time: 5 ms (41.53%), Space: 96.8 MB (25.87%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,39 +10,44 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
+
+        if(head == null || head.next == null) return true;
+
         ListNode slow = head;
         ListNode fast = head;
-
+        // Finding the middle of the LL & divide the LL into two halves
         while(fast.next != null && fast.next.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
 
+        // Reverse the second half only
         ListNode newHead = reverseLL(slow.next);
         ListNode first = head;
         ListNode second = newHead;
         while(second != null){
             if(first.val != second.val){
-                reverseLL(newHead);
+                reverseLL(newHead); // Re-reverse the 2nd half before returning 
                 return false;
             }
             first = first.next;
             second = second.next;
         }
-        reverseLL(newHead);
+        reverseLL(newHead); // Re-reverse the 2nd half before returning
         return true;
     }
 
-    private ListNode reverseLL(ListNode head) {
-        // Base Case
-        if(head == null || head.next == null){
-            return head;
-        }
-        ListNode newHead = reverseLL(head.next);
-        ListNode front = head.next;
-        front.next = head;
-        head.next = null;
+    // Iterative approach for reversing a LL
+    public ListNode reverseLL(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
 
-        return newHead;
+        while(temp != null){
+            ListNode front = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
     }
 }
