b73cb406fedea036f0b4e86d05b9617271c2bab7
Krishnendu Raut <163745847+Saheb20004@users.noreply.github.com>
2026-09-15 23:26:23 +0530

    Time: 1 ms (100%), Space: 42.2 MB (100%) - LeetHub

--- 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
+++ 2472-maximum-number-of-non-overlapping-palindrome-substrings/2472-maximum-number-of-non-overlapping-palindrome-substrings.java
@@ -1,29 +1,32 @@
 class Solution {
     public int maxPalindromes(String s, int k) {
         int n = s.length();
-        boolean[][] isPal = new boolean[n][n];
-        
-        for (int i = 0; i < n; ++i)
-            isPal[i][i] = true;
-        
-        for (int i = 0; i + 1 < n; ++i)
-            if (s.charAt(i) == s.charAt(i + 1))
-                isPal[i][i + 1] = true;
-        
-        for (int len = 3; len <= n; ++len)
-            for (int i = 0; i + len - 1 < n; ++i) {
-                int j = i + len - 1;
-                if (s.charAt(i) == s.charAt(j) && isPal[i + 1][j - 1])
-                    isPal[i][j] = true;
+        // Edge Case
+        if (k == 1)
+            return n;
+
+        int count = 0;
+
+        for (int i = 0; i <= n-k; i++) {
+            // Check palindrome of length k
+            if (isPalindrome(s, i, i + k-1)) {
+                count ++;
+                i += k-1;
+            } 
+            // Check palindrome of length k + 1
+            else if (i < n-k && isPalindrome(s, i, i + k)) {
+                count ++;
+                i += k;
             }
-        
-        int[] dp = new int[n + 1];
-        for (int i = 1; i <= n; ++i) {
-            dp[i] = dp[i - 1];
-            for (int j = 0; j <= i - k; ++j)
-                if (isPal[j][i - 1])
-                    dp[i] = Math.max(dp[i], dp[j] + 1);
         }
-        return dp[n];
+
+        return count;
+    }
+
+    private boolean isPalindrome(String s, int left, int right) {
+        for (; left < right;  left ++,right --)
+            if (s.charAt(left) != s.charAt(right))
+                return false;
+        return true;
     }
 }
